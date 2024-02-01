583c259e1502588b8004ba7064c52b818af1d980
dborg <nebukader@gmail.com>
2024-02-01 16:38:20 +0100

    Implementierung von FirebaseAuth und LiveData Observer im HomeFragment, Hinzufügung von Debugging-Button dataBT - Recycler view mit entsprechendem Adapter angebunden und LiveData versorgt

--- app/src/main/java/com/example/brush_wisperer/ui/HomeFragment/HomeFragment.kt
+++ app/src/main/java/com/example/brush_wisperer/ui/HomeFragment/HomeFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.brush_wisperer.R
 import com.example.brush_wisperer.databinding.FragmentHomeBinding
+import com.example.brush_wisperer.ui.Adapter.NewsAdapter
 import com.google.firebase.auth.FirebaseAuth
 
 
 class HomeFragment : Fragment() {
+
+    private val viewModel: HomeViewModel by activityViewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,22 +25,29 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
 
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentHomeBinding.bind(view)
 
+
+        binding.dataBT.setOnClickListener {
+            viewModel.insertNews()
+        }
+
         val user = FirebaseAuth.getInstance().currentUser
         user?.let {
             val name = user.displayName
-            // Verwenden Sie hier die Benutzerdaten
             binding.usernameTV.text = "$name"
         }
-        binding.dataBT.setOnClickListener {
-            HomeViewModel().scrapeWebPage()
-            HomeViewModel().scrapeBlogPost("/blogs/explore/tagged/news")
 
 
+        viewModel.news.observe(viewLifecycleOwner) { news ->
+            news?.let {
+                binding.newsRecyclerView.adapter = NewsAdapter(viewModel, news)
+            }
         }
+
     }
 
 }
