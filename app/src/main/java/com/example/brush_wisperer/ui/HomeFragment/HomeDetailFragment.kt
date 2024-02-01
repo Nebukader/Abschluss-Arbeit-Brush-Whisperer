4c04baa70c82d89b797b5433d0b1e28a2a6d487b
dborg <nebukader@gmail.com>
2024-02-01 16:36:00 +0100

    Implementierung von WebView und Safe Args im Home Detail Fragment und Hinzufügung der Safe Args Abhängigkeiten

---
+++ app/src/main/java/com/example/brush_wisperer/ui/HomeFragment/HomeDetailFragment.kt
@@ -0,0 +1,30 @@
+package com.example.brush_wisperer.ui.HomeFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import com.example.brush_wisperer.R
+import com.example.brush_wisperer.databinding.FragmentHomeDetailBinding
+
+class HomeDetailFragment : Fragment() {
+    private val args: HomeDetailFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home_detail, container, false)
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentHomeDetailBinding.bind(view)
+        binding.webView.loadUrl(args.postLink)
+
+
+    }
+}
