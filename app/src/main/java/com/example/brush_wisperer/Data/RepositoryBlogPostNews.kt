0001e4c6a22d8bb47584926c4bb9f8b0f75fa481
dborg <nebukader@gmail.com>
2024-02-01 16:43:18 +0100

    -web scraping Repo angelegt zum verwalten der Daten

---
+++ app/src/main/java/com/example/brush_wisperer/Data/RepositoryBlogPostNews.kt
@@ -0,0 +1,53 @@
+package com.example.brush_wisperer.Data
+
+import android.util.Log
+import com.example.brush_wisperer.Data.Local.Model.BlogPostEntity
+import com.example.brush_wisperer.Data.Local.Model.Database.BlogPostDatabase
+import org.jsoup.Jsoup
+
+class RepositoryBlogPostNews(private val database: BlogPostDatabase) {
+
+    val allNews = database.dao.getAllNews()
+
+    suspend fun insertNews(news: BlogPostEntity) {
+        database.dao.insertBlogPost(news)
+    }
+
+    fun scrapeWebPage() {
+        val doc = Jsoup.connect("https://thearmypainter.com/blogs/explore").get()
+        val elements = doc.select("li[role=tab] a")
+        elements.forEach { element ->
+            val postLink = element.attr("href")
+            Log.d("TAG", "Post link: $postLink")
+        }
+    }
+
+
+    fun scrapeBlogPost(postLink: String): List<BlogPostEntity> {
+        val doc = Jsoup.connect("https://thearmypainter.com$postLink").get()
+        val blogPost = doc.select("blog-post-card")
+        val blogPostEntities = mutableListOf<BlogPostEntity>()
+        blogPost.forEach { post ->
+            val title = post.select("a.blog-post-card__figure img").attr("alt")
+            val image = post.select("img.zoom-image").attr("src")
+            val text = post.select("p").text()
+
+            val postLink = post.select("a.blog-post-card__figure").attr("href")
+            val processedPostLink = "https://thearmypainter.com$postLink"
+
+            val processedImageLink = "https:${image.substringAfter("Image: ").substringBefore("?") }"
+
+            val news = BlogPostEntity(
+                0,
+                title,
+                processedImageLink,
+                text,
+                processedPostLink
+            )
+            blogPostEntities.add(news)
+            Log.d("TAG", "PostLink : $postLink")
+        }
+        return blogPostEntities
+    }
+}
+
