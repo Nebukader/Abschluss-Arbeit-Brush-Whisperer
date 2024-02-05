73c91f3397a32ba99e6054e607090b3ff0d4b569
dborg <nebukader@gmail.com>
2024-02-05 10:40:13 +0100

    Angepasst das nach dem Einloggen die Bottom Nav angezeigt wird

--- app/src/main/java/com/example/brush_wisperer/MainActivity.kt
+++ app/src/main/java/com/example/brush_wisperer/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.brush_wisperer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.color.DynamicColors
+import android.view.View
+import com.example.brush_wisperer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        }
+        private lateinit var binding: ActivityMainBinding
 
+        override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                binding = ActivityMainBinding.inflate(layoutInflater)
+                setContentView(binding.root)
+
+                binding.bottomNavigationView.visibility = View.GONE
+        }
+        fun showBottomNav(visibility: Int){
+                binding.bottomNavigationView.visibility = visibility
+        }
 }
