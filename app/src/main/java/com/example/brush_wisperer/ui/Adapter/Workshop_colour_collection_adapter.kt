44daaec5afb85e692b14e96d8196f76f4ef82239
Dennis <Nebukader@gmail.com>
2024-02-15 21:31:32 +0100

    -farben die in der colour list als favourite gesetzt werden, werden in firestore gespeichert, und wieder abgerufen in der colour collection und von firestore geladen

--- app/src/main/java/com/example/brush_wisperer/ui/Adapter/Workshop_colour_collection_adapter.kt
+++ app/src/main/java/com/example/brush_wisperer/ui/Adapter/Workshop_colour_collection_adapter.kt
@@ -1,25 +1,19 @@
 package com.example.brush_wisperer.ui.Adapter
 
-import android.annotation.SuppressLint
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.brush_wisperer.Data.Local.Model.ColourEntity
+import com.example.brush_wisperer.Data.Model.firestoreColour
 import com.example.brush_wisperer.databinding.WorkshopMyCollectionColourItemBinding
 
 class Workshop_colour_collection_adapter(
 
-    private var colourCollection: List<ColourEntity>
+    private val favoritColourList : ArrayList<firestoreColour>
 
 ) : RecyclerView.Adapter<Workshop_colour_collection_adapter.ItemViewHolder>() {
 
-    @SuppressLint("NotifyDataSetChanged")
-    fun submitList(list: List<ColourEntity>) {
-        colourCollection = list
-        notifyDataSetChanged()
-    }
 
     class ItemViewHolder(val binding: WorkshopMyCollectionColourItemBinding) :
         RecyclerView.ViewHolder(binding.root)
@@ -29,23 +23,18 @@ class Workshop_colour_collection_adapter(
         return ItemViewHolder(binding)
     }
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val colourData = colourCollection
+        val colourData = favoritColourList
         val binding = holder.binding
 
-        val color = Color.parseColor(colourData[position].hexcode)
+        val color = Color.parseColor(colourData[position].hexCode)
         val colorSateList = ColorStateList.valueOf(color)
         binding.colourShapeIV.imageTintList = colorSateList
-        binding.colourNameTV.text = colourData[position].colour_name
-        binding.colourRangeTV.text = colourData[position].colour_range
+        binding.colourNameTV.text = colourData[position].colourName
+        binding.colourRangeTV.text = colourData[position].colourRange
 
 
 
     }
-    fun onApplySearch(colourCollection: List<ColourEntity>){
 
-        this.colourCollection = colourCollection
-        notifyDataSetChanged()
-    }
-
-    override fun getItemCount() = colourCollection.size
+    override fun getItemCount() = favoritColourList.size
 }
--- app/src/main/java/com/example/brush_wisperer/ui/MyWorkshopFragment/Workshop_my_colour_collection.kt
+++ app/src/main/java/com/example/brush_wisperer/ui/MyWorkshopFragment/Workshop_my_colour_collection.kt
@@ -1,19 +1,31 @@
 package com.example.brush_wisperer.ui.MyWorkshopFragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.example.brush_wisperer.R
+import com.example.brush_wisperer.Data.Model.firestoreColour
 import com.example.brush_wisperer.databinding.FragmentWorkshopMyColourCollectionBinding
+import com.example.brush_wisperer.ui.Adapter.Workshop_colour_collection_adapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
 
 class Workshop_my_colour_collection : Fragment() {
 
-    private lateinit var viewModel: WorkshopViewModel
+    private val viewModel: WorkshopViewModel by activityViewModels()
     private lateinit var binding: FragmentWorkshopMyColourCollectionBinding
+    private lateinit var favouriteColoursArrayList: ArrayList<firestoreColour>
+    private lateinit var db: FirebaseFirestore
+    private lateinit var adapter: Workshop_colour_collection_adapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +34,9 @@ class Workshop_my_colour_collection : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentWorkshopMyColourCollectionBinding.inflate(inflater)
+        favouriteColoursArrayList = ArrayList()
+        adapter = Workshop_colour_collection_adapter(favouriteColoursArrayList)
+
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -32,7 +47,34 @@ class Workshop_my_colour_collection : Fragment() {
             findNavController().popBackStack()
             findNavController().navigate(Workshop_my_colour_collectionDirections.actionWorkshopMyColourCollectionToWorkshopColourList())
         }
+        val currentUser = FirebaseAuth.getInstance().currentUser?.uid
 
+        // get the user's favourite colours
+
+        fun getFavouriteColours() {
+            val currentUserId = FirebaseAuth.getInstance().currentUser?.uid.toString()
+            db = FirebaseFirestore.getInstance()
+            db.collection("users").document(currentUserId).collection("colours").addSnapshotListener(object : EventListener<QuerySnapshot> {
+                override fun onEvent(
+                    value: QuerySnapshot?,
+                    error: FirebaseFirestoreException?
+                ){
+                    if (error != null) {
+                        Log.e("Firestore Error",  error.message.toString())
+                        return
+                    }
+                    for (dc : DocumentChange in value?.documentChanges!!) {
+
+                        if (dc.type == DocumentChange.Type.ADDED) {
+                            favouriteColoursArrayList.add(dc.document.toObject(firestoreColour::class.java))
+                        }
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+            })
+        }
+        getFavouriteColours()
+        binding.myColourCollectionRecyclerView.adapter = adapter
         // Setup Spinner
         val spinnerList = arrayOf("The Army Painter", "Games Workshop", "Vallejo","AK Interactive")
         val spinnerAdapter = context?.let { ArrayAdapter(it, android.R.layout.simple_spinner_dropdown_item, spinnerList) }
