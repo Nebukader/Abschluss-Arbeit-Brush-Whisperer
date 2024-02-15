b47ed63a8998abbb4b17e4ef3247b83e27d6dadc
Dennis <Nebukader@gmail.com>
2024-02-15 21:34:34 +0100

    -data class zum speichern und laden für firestore angelegt

--- app/src/main/java/com/example/brush_wisperer/Data/Model/firestoreColour.kt
+++ app/src/main/java/com/example/brush_wisperer/Data/Model/firestoreColour.kt
@@ -3,10 +3,10 @@ package com.example.brush_wisperer.Data.Model
 
 
 data class firestoreColour(
-    val id: String,
-    val brandName: String,
-    val colourRange: String,
-    val colourPrimary: String,
-    val colourName: String,
-    val hexCode: String,
+    val id: String = "",
+    val brandName: String = "",
+    val colourRange: String = "",
+    val colourPrimary: String = "",
+    val colourName: String = "",
+    val hexCode: String = "",
 )
