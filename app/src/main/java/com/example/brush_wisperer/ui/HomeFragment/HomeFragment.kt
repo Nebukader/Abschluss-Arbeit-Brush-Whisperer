47ea1fc062363f3faf0e5fb1926cc41e05d1fced
Dennis <Nebukader@gmail.com>
2024-02-15 21:34:51 +0100

    -update

--- app/src/main/java/com/example/brush_wisperer/ui/HomeFragment/HomeFragment.kt
+++ app/src/main/java/com/example/brush_wisperer/ui/HomeFragment/HomeFragment.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.brush_wisperer.R
@@ -38,6 +38,7 @@ class HomeFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
 
         swipeRefreshLayout = binding.swipeRefreshLayout
         swipeRefreshLayout.setOnRefreshListener(this)
+
 
         viewModel.updateNews()
 
