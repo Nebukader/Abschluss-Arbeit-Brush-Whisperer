0fe19c168aa75eca1b7f0b93aefd3d9d78fce672
Dennis <Nebukader@gmail.com>
2024-02-21 14:41:29 +0100

    - Datenklasse für Projekte angelegt

---
+++ app/src/main/java/com/example/brush_wisperer/Data/Model/Projects.kt
@@ -0,0 +1,8 @@
+package com.example.brush_wisperer.Data.Model
+
+data class Projects(
+    val title: String ="",
+    val description: String ="",
+    val miniatures: Int=0,
+    val image: String=""
+)
