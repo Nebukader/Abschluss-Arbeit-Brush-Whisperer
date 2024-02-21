15a463c620d4e5b6d39dbad8ec6ad40865e966f9
Dennis <Nebukader@gmail.com>
2024-02-21 14:44:06 +0100

    - update

--- app/src/main/java/com/example/brush_wisperer/ui/ColourFragment/ColourRangesAndColourListFragment.kt
+++ app/src/main/java/com/example/brush_wisperer/ui/ColourFragment/ColourRangesAndColourListFragment.kt
@@ -1,29 +1,27 @@
 package com.example.brush_wisperer.ui.ColourFragment
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.brush_wisperer.Data.Local.Model.ColourEntity
+import com.example.brush_wisperer.Data.Model.ColourList
 import com.example.brush_wisperer.R
 import com.example.brush_wisperer.databinding.FragmentColourRangesAndColourListBinding
 import com.example.brush_wisperer.ui.Adapter.ColourListAdapter
-import androidx.appcompat.widget.SearchView
-import androidx.appcompat.widget.SearchView.*
-import androidx.lifecycle.Observer
-import com.example.brush_wisperer.Data.Local.Model.ColourEntity
-import com.example.brush_wisperer.Data.Model.ColourList
 import java.util.Locale
+
 
 
 class ColourRangesAndColourListFragment : Fragment() {
 
     private val colourItemList = ArrayList<ColourList>()
+    private var isDataLoaded = false
 
     private val viewModel: ColourViewModel by viewModels()
     private lateinit var binding: FragmentColourRangesAndColourListBinding
@@ -41,10 +39,11 @@ class ColourRangesAndColourListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = ColourListAdapter(emptyList(), viewModel)
+        val adapter = ColourListAdapter(viewModel)
         val recyclerView = binding.colourRangesAndColourListRV
 
         recyclerView.adapter = adapter
+
 
         viewModel.loading.observe(viewLifecycleOwner) {
             when (it) {
@@ -60,46 +59,50 @@ class ColourRangesAndColourListFragment : Fragment() {
                 }
             }
         }
-        viewModel.colourList.observe(viewLifecycleOwner) {
-            adapter.submitList(it)
-
-            //Starten der Animation beim ersten Laden
-            val context = binding.colourRangesAndColourListRV.context
-            val controller = AnimationUtils.loadLayoutAnimation(context, R.anim.layout_animation)
-
-            binding.colourRangesAndColourListRV.layoutAnimation = controller
-            binding.colourRangesAndColourListRV.scheduleLayoutAnimation()
-
-            val searchView = binding.searchBar
-
-            searchView.setOnQueryTextListener(object : OnQueryTextListener {
-                override fun onQueryTextSubmit(query: String?): Boolean {
-                    return false
-                }
-
-                override fun onQueryTextChange(newText: String?): Boolean {
-
-                    val searchList = ArrayList<ColourEntity>()
-
-                    if (newText != null) {
-                        for (item in viewModel.colourList.value!!) {
-                            val lowerCaseNewText = newText.lowercase(Locale.ROOT)
-                            if (item.colour_name.lowercase(Locale.ROOT).contains(lowerCaseNewText)) {
-                                searchList.add(item)
-                            }
-                        }
-                        if (searchList.isEmpty()){
-                            Toast.makeText(context, "No Colour found", Toast.LENGTH_SHORT).show()
-                        }else{
-                            adapter.onApplySearch(searchList)
-                        }
-                    }
-                    return true
-                }
-            })
+        viewModel.colourList.observe(viewLifecycleOwner) { colourList ->
+            if (!isDataLoaded) {
+                adapter.submitList(colourList)
+                isDataLoaded = true
+            }
         }
 
+        //Starten der Animation beim ersten Laden
+        val context = binding.colourRangesAndColourListRV.context
+        val controller = AnimationUtils.loadLayoutAnimation(context, R.anim.layout_animation)
+
+        binding.colourRangesAndColourListRV.layoutAnimation = controller
+        binding.colourRangesAndColourListRV.scheduleLayoutAnimation()
+
+        val searchView = binding.searchBar
+
+        searchView.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                val searchList = ArrayList<ColourEntity>()
+
+                if (newText != null) {
+                    for (item in viewModel.colourList.value!!) {
+                        val lowerCaseNewText = newText.lowercase(Locale.ROOT)
+                        if (item.colour_name.lowercase(Locale.ROOT).contains(lowerCaseNewText)) {
+                            searchList.add(item)
+                        }
+                    }
+                    if (searchList.isEmpty()) {
+                        Toast.makeText(context, "No Colour found", Toast.LENGTH_SHORT).show()
+                    } else {
+                        adapter.onApplySearch(searchList)
+                    }
+                }
+                return true
+            }
+        })
     }
 
 }
+
+
 
