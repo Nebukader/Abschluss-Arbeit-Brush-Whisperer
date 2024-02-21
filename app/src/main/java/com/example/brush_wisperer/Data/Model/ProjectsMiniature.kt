e546338fa8f71767047735efed2d9ef4f7a1df09
Dennis <Nebukader@gmail.com>
2024-02-21 14:32:23 +0100

    - datenklasse für Miniatures angelegt

---
+++ app/src/main/java/com/example/brush_wisperer/Data/Model/ProjectsMiniature.kt
@@ -0,0 +1,8 @@
+package com.example.brush_wisperer.Data.Model
+
+class ProjectsMiniature (
+    val miniName: String = "",
+    val miniImage: String = "",
+    var currentAmmount: Int = 1,
+    val miniAmmount: Int = 0
+)
