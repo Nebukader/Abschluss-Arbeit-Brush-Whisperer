adcf0c3e955da934c0481a4d2c867a104c4887d0
Dennis <Nebukader@gmail.com>
2024-02-21 10:45:18 +0100

    - adapter angebunden an die recyclerview - damit der observer nicht wieder ausgelöst wird wenn die livedata sich ändert wurde eine variable angelegt das die livedata nicht jedes mal aktualisiert wird

--- app/src/main/java/com/example/brush_wisperer/ui/MyWorkshopFragment/Workshop_colour_list.kt
+++ app/src/main/java/com/example/brush_wisperer/ui/MyWorkshopFragment/Workshop_colour_list.kt
@@ -1,20 +1,24 @@
 package com.example.brush_wisperer.ui.MyWorkshopFragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.brush_wisperer.R
 import com.example.brush_wisperer.databinding.FragmentWorkshopColourListBinding
+import com.example.brush_wisperer.ui.Adapter.ColourListAdapter
 import com.example.brush_wisperer.ui.ColourFragment.ColourViewModel
 
 class Workshop_colour_list : Fragment() {
 
     private val viewModel: ColourViewModel by viewModels()
     private lateinit var binding: FragmentWorkshopColourListBinding
+    private var isDataLoaded = false
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +32,22 @@ class Workshop_colour_list : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ColourListAdapter(viewModel)
+        val recyclerView = binding.workshopColourListRV
+
+        recyclerView.adapter = adapter
+
+        viewModel.colourList.observe(viewLifecycleOwner) {
+            if (!isDataLoaded){
+                adapter.submitList(it)
+                isDataLoaded = true
+            }
+        }
+
+        val context = binding.workshopColourListRV.context
+        val controller = AnimationUtils.loadLayoutAnimation(context, R.anim.layout_animation)
+        binding.workshopColourListRV.scheduleLayoutAnimation()
 
         // Setup Spinner
         val spinnerList = arrayOf("The Army Painter", "Games Workshop", "Vallejo","AK Interactive")
