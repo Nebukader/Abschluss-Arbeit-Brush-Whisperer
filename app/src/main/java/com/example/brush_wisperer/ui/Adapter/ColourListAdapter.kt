2ee6d059cb7821df35f64c5538c66bf2e1a8f334
Dennis <Nebukader@gmail.com>
2024-02-21 14:44:23 +0100

    - update

--- app/src/main/java/com/example/brush_wisperer/ui/Adapter/ColourListAdapter.kt
+++ app/src/main/java/com/example/brush_wisperer/ui/Adapter/ColourListAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.brush_wisperer.ui.Adapter
 
-import android.annotation.SuppressLint
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.brush_wisperer.Data.Local.Model.ColourEntity
@@ -13,20 +15,12 @@ import com.example.brush_wisperer.databinding.ColourItemBinding
 import com.example.brush_wisperer.ui.ColourFragment.ColourViewModel
 import com.google.firebase.auth.FirebaseAuth
 
-class ColourListAdapter (
-
-    private var colourList: List<ColourEntity>,
+class ColourListAdapter(
     private val viewModel: ColourViewModel
+) : ListAdapter<ColourEntity, ColourListAdapter.ItemViewHolder>(ColourDiffCallback()) {
 
-) : RecyclerView.Adapter<ColourListAdapter.ItemViewHolder>() {
-
-    @SuppressLint("NotifyDataSetChanged")
-    fun submitList(list: List<ColourEntity>) {
-        colourList = list
-        notifyDataSetChanged()
-    }
     class ItemViewHolder(val binding: ColourItemBinding) :
-    RecyclerView.ViewHolder(binding.root)
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val binding = ColourItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -34,54 +28,63 @@ class ColourListAdapter (
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val colourData = colourList
+        val colourData = getItem(position)
         val binding = holder.binding
 
-        val color = Color.parseColor(colourData[position].hexcode)
+        val color = Color.parseColor(colourData.hexcode)
         val colorStateList = ColorStateList.valueOf(color)
 
         binding.colourShapeIV.imageTintList = colorStateList
-        binding.colourNameTV.text = colourData[position].colour_name
-        val image = "http://dockerpi/back/${colourData[position].picture}"
+        binding.colourNameTV.text = colourData.colour_name
+        val image = "http://dockerpi/back/${colourData.picture}"
         binding.colourItemIV.load(image)
 
-        if (colourData[position].isFavorite) {
+        if (colourData.isFavorite) {
             binding.colourAddToCollectionBtn.setImageResource(R.drawable.favorit_selected)
         } else {
             binding.colourAddToCollectionBtn.setImageResource(R.drawable.favorit_not_selected)
         }
 
-        val documentid = FirebaseAuth.getInstance().currentUser?.uid.toString()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val documentid = currentUser?.uid.toString()
+        Log.d("TAG", "Adapter: ${currentUser?.uid}")
+
         binding.colourAddToCollectionBtn.setOnClickListener {
-            if (colourData[position].isFavorite) {
+            if (!colourData.isFavorite) {
                 binding.colourAddToCollectionBtn.setImageResource(R.drawable.favorit_selected)
-                viewModel.updateFavourite(colourData[position].id, true)
+                viewModel.updateFavourite(colourData.id, true)
                 viewModel.saveColour(
                     documentid,
-                    colourData[position].id.toString(),
-                    colourData[position].brand_name,
-                    colourData[position].colour_range,
-                    colourData[position].colour_primary,
-                    colourData[position].colour_name,
-                    colourData[position].hexcode
+                    colourData.id.toString(),
+                    colourData.brand_name,
+                    colourData.colour_range,
+                    colourData.colour_primary,
+                    colourData.colour_name,
+                    colourData.hexcode
                 )
             } else {
                 binding.colourAddToCollectionBtn.setImageResource(R.drawable.favorit_not_selected)
-                viewModel.updateFavourite(colourData[position].id, false)
+                viewModel.updateFavourite(colourData.id, false)
                 viewModel.deleteColour(
                     documentid,
-                    colourData[position].id.toString()
+                    colourData.id.toString()
                 )
             }
         }
 
     }
-    fun onApplySearch(colourList: List<ColourEntity>){
 
-        this.colourList = colourList
-        notifyDataSetChanged()
+    fun onApplySearch(colourList: List<ColourEntity>) {
+        submitList(colourList)
+    }
+}
+
+class ColourDiffCallback : DiffUtil.ItemCallback<ColourEntity>() {
+    override fun areItemsTheSame(oldItem: ColourEntity, newItem: ColourEntity): Boolean {
+        return oldItem.id == newItem.id
     }
 
-
-    override fun getItemCount() = colourList.size
+    override fun areContentsTheSame(oldItem: ColourEntity, newItem: ColourEntity): Boolean {
+        return oldItem == newItem
+    }
 }
