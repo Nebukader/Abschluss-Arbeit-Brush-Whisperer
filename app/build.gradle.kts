e9951392caab36a799269275acde6304b201ed95
Dennis <Nebukader@gmail.com>
2024-02-21 09:59:38 +0100

    -firestorage hinzugefügt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,9 +48,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation("com.google.firebase:firebase-firestore:24.10.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("com.google.firebase:firebase-firestore:24.10.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
@@ -58,7 +58,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("androidx.compose.material3:material3:1.1.2")
+    implementation ("androidx.compose.material3:material3:1.2.0")
     implementation ("com.google.android.material:material:1.11.0")
 
 
@@ -68,6 +68,8 @@ dependencies {
     implementation ("com.google.android.gms:play-services-auth")
     implementation ("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
+
 
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
@@ -80,17 +82,17 @@ dependencies {
     implementation ("androidx.room:room-runtime:2.6.1")
     implementation ("androidx.room:room-ktx:2.6.1")
     ksp ("androidx.room:room-compiler:2.6.1")
-
     // Jsoup Dependency for Web Scraping
     implementation("org.jsoup:jsoup:1.14.3")
-
     //SwipeToRefresh
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-
     //Worker
     implementation("androidx.work:work-runtime-ktx:2.9.0")
-
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-
-
+    // CameraX core library using camera2 implementation
+    implementation ("androidx.camera:camera-camera2:1.3.1")
+    // CameraX Lifecycle library
+    implementation ("androidx.camera:camera-lifecycle:1.3.1")
+    // CameraX View class
+    implementation ("androidx.camera:camera-view:1.3.1")
 }
