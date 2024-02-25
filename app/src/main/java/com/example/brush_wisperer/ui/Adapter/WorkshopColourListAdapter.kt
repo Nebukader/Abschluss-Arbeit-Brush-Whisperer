fa21eb631c55c84622095543390a4d399e26a053
dennis <borges.dennis@icloud.com>
2024-02-25 23:58:36 +0100

    -version angebpasst das es für tutoren ausführbar ist

--- app/src/main/java/com/example/brush_wisperer/ui/Adapter/WorkshopColourListAdapter.kt
+++ app/src/main/java/com/example/brush_wisperer/ui/Adapter/WorkshopColourListAdapter.kt
@@ -1,91 +1,61 @@
 package com.example.brush_wisperer.ui.Adapter
 
+import android.app.AlertDialog
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.brush_wisperer.Data.Local.Model.ColourEntity
+import com.example.brush_wisperer.Data.Model.Projects
 import com.example.brush_wisperer.R
 import com.example.brush_wisperer.databinding.ColourItemBinding
 import com.example.brush_wisperer.databinding.WorkshopPopupColourlistAddItemBinding
+import com.example.brush_wisperer.databinding.WorkshopProjectItemBinding
 import com.example.brush_wisperer.ui.ColourFragment.ColourViewModel
 import com.example.brush_wisperer.ui.WorkshopFragment.WorkshopViewModel
+import com.example.brush_wisperer.ui.WorkshopFragment.Workshop_projectDirections
 import com.google.firebase.auth.FirebaseAuth
 
 class WorkshopColourListAdapter(
-    private val viewModel: WorkshopViewModel
-) : ListAdapter<ColourEntity, WorkshopColourListAdapter.ItemViewHolder>(ColourDiffCallBack()) {
+    private val miniatureColour: ArrayList<ColourEntity>,
+    private val viewModel : WorkshopViewModel,
+) : RecyclerView.Adapter<WorkshopColourListAdapter.ItemViewHolder>() {
 
-        class ItemViewHolder(val binding: WorkshopPopupColourlistAddItemBinding) :
-            RecyclerView.ViewHolder(binding.root)
+    class ItemViewHolder(val binding: WorkshopPopupColourlistAddItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-            val binding = WorkshopPopupColourlistAddItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            return ItemViewHolder(binding)
-        }
-
-        override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-            val binding = holder.binding
-            val colourData = Fir
-
-            val color = Color.parseColor(colourData.hexcode)
-            val colorStateList = ColorStateList.valueOf(color)
-
-            binding.colourShapeIV.imageTintList = colorStateList
-            binding.colourNameTV.text =
-
-
-            if (colourData.isFavorite) {
-                binding.colourAddToMiniBTN.setImageResource(R.drawable.favorit_selected)
-            } else {
-                binding.colourAddToMiniBTN.setImageResource(R.drawable.favorit_not_selected)
-            }
-
-            val currentUser = viewModel.firebaseCurrentUserID()
-            val documentid = currentUser.toString()
-
-            binding.colourAddToMiniBTN.setOnClickListener {
-                if (!colourData.isFavorite) {
-                    binding.colourAddToMiniBTN.setImageResource(R.drawable.favorit_selected)
-                    viewModel.updateFavourite(colourData.id, true)
-                    viewModel.saveColour(
-                        documentid,
-                        colourData.id.toString(),
-                        colourData.brand_name,
-                        colourData.colour_range,
-                        colourData.colour_primary,
-                        colourData.colour_name,
-                        colourData.hexcode
-                    )
-                } else {
-                    binding.colourAddToCollectionBtn.setImageResource(R.drawable.favorit_not_selected)
-                    viewModel.updateFavourite(colourData.id, false)
-                    viewModel.deleteColour(
-                        documentid,
-                        colourData.id.toString()
-                    )
-                }
-            }
-
-        }
-
-    fun onApplySearch(colourList: List<ColourEntity>) {
-        submitList(colourList)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val binding = WorkshopPopupColourlistAddItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemViewHolder(binding)
     }
+
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+
+        val userID = viewModel.firebaseCurrentUserID()
+        val db = viewModel.currentUserDB()
+        // Richtigen Pfad setzten
+        val docRef = db.collection("users").document(userID!!).collection("projects").document(miniatureColour[position].colour_name)
+        val binding = holder.binding
+        val colourName = miniatureColour[position].colour_name
+        val colourHex = miniatureColour[position].hexcode
+        binding.colourNameTV.text = miniatureColour[position].colour_name
+        binding.colourShapeIV
+
+    }
+
+
+
+    override fun getItemCount() = miniatureColour.size
 }
 
-// Only one declaration of ColourDiffCallback class
-class ColourDiffCallBack : DiffUtil.ItemCallback<ColourEntity>() {
-    override fun areItemsTheSame(oldItem: ColourEntity, newItem: ColourEntity): Boolean {
-        return oldItem.id == newItem.id
-    }
-
-    override fun areContentsTheSame(oldItem: ColourEntity, newItem: ColourEntity): Boolean {
-        return oldItem == newItem
-    }
-}
