47f75e28524ee9d2708c14fbbada99d297965ad4
dennis <borges.dennis@icloud.com>
2024-03-05 14:00:17 +0100

    -imports optimiert

--- app/src/main/java/com/example/brush_wisperer/ui/HomeFragment/HomeDetailFragment.kt
+++ app/src/main/java/com/example/brush_wisperer/ui/HomeFragment/HomeDetailFragment.kt
@@ -1,16 +1,13 @@
 package com.example.brush_wisperer.ui.HomeFragment
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.example.brush_wisperer.Data.RepositoryFirebase
 import com.example.brush_wisperer.R
 import com.example.brush_wisperer.databinding.FragmentHomeDetailBinding
-import kotlinx.coroutines.coroutineScope
 
 class HomeDetailFragment : Fragment() {
     private val args: HomeDetailFragmentArgs by navArgs()
