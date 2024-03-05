84001d19e84300fd72e94999786744bf8190327f
dennis <borges.dennis@icloud.com>
2024-03-05 14:01:46 +0100

    - url angepasst

--- app/src/main/java/com/example/brush_wisperer/Data/Remote/ColourAPI.kt
+++ app/src/main/java/com/example/brush_wisperer/Data/Remote/ColourAPI.kt
@@ -1,14 +1,13 @@
 package com.example.brush_wisperer.Data.Remote
 
 import com.example.brush_wisperer.Data.Local.Model.ColourEntity
-import com.example.brush_wisperer.Data.Model.ColourList
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-const val BASE_URL = "http://brushwhisperer.ddns.net:9474/back/api/"
+const val BASE_URL = "http://brushwhisperer.ddns.net:9475/back/api/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
