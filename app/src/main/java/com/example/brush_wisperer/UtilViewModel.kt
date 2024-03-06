fac491f7780a4fd2ab161289ca9608905113940f
dennis <borges.dennis@icloud.com>
2024-03-06 19:17:54 +0100

    -refractor name

--- app/src/main/java/com/example/brush_wisperer/UtilViewModel.kt
+++ app/src/main/java/com/example/brush_wisperer/UtilViewModel.kt
@@ -6,7 +6,7 @@ import android.view.Gravity
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 
-class MainViewModel(application: Application) : AndroidViewModel(application) {
+class UtilViewModel(application: Application) : AndroidViewModel(application) {
 
     fun showToastAtTop(context: Context, message: String) {
         val toast = Toast.makeText(context, message, Toast.LENGTH_SHORT)
